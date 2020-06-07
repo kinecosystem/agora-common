ee34b20e474f57fa46ecebe53ee2700563bfd19e
Kik Engineering <engineering@kik.com>
2020-06-07 15:58:31 -0400

    aws/dynamodb/test: support testing against the v1 sdk
    GitOrigin-RevId: 35d35cf605d8f19d278d663a1ab365c55f0bbc98

--- aws/dynamodb/test/util.go
+++ aws/dynamodb/test/util.go
@@ -9,6 +9,11 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	dynamodbv1 "github.com/aws/aws-sdk-go/service/dynamodb"
+	dynamodbifacev1 "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 	"github.com/ory/dockertest"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -56,6 +61,56 @@ func StartDynamoDB(pool *dockertest.Pool) (db dynamodbiface.ClientAPI, closeFunc
 		func() error {
 			_, err := db.ListTablesRequest(&dynamodb.ListTablesInput{}).Send(context.Background())
 			return err
+		},
+		retry.Limit(20),
+		retry.Backoff(backoff.Constant(500*time.Millisecond), 500*time.Second),
+	)
+	if err != nil {
+		return nil, closeFunc, errors.Wrap(err, "timed out waiting for dynamodb container to become available")
+	}
+
+	return db, closeFunc, nil
+}
+
+// StartDynamoDBV1 starts a Docker container using the dynamodb-local image and returns a V1 DynamoDB client for testing purposes.
+func StartDynamoDBV1(pool *dockertest.Pool) (db dynamodbifacev1.DynamoDBAPI, closeFunc func(), err error) {
+	closeFunc = func() {}
+
+	resource, err := pool.Run(containerName, containerVersion, nil)
+	if err != nil {
+		return nil, closeFunc, errors.Wrapf(err, "failed to start resource")
+	}
+
+	closeFunc = func() {
+		if err := pool.Purge(resource); err != nil {
+			logrus.StandardLogger().WithError(err).Warn("Failed to cleanup dynamodb resource")
+		}
+	}
+
+	address := resource.GetHostPort("8000/tcp")
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, closeFunc, errors.Wrapf(err, "failed to load default aws config")
+	}
+
+	resolver := func(service, region string, optFns ...func(*endpoints.Options)) (endpoints.ResolvedEndpoint, error) {
+		return endpoints.ResolvedEndpoint{
+			URL: fmt.Sprintf("http://%s", address),
+		}, nil
+	}
+
+	// Ensure that clients never reach out to a real system
+	sess.Config.Region = aws.String("test-region-1")
+	sess.Config.Credentials = credentials.NewStaticCredentials("test", "test", "test")
+	sess.Config.EndpointResolver = endpoints.ResolverFunc(resolver)
+
+	db = dynamodbv1.New(sess)
+
+	_, err = retry.Retry(
+		func() error {
+			req, _ := db.ListTablesRequest(&dynamodbv1.ListTablesInput{})
+			return req.Send()
 		},
 		retry.Limit(20),
 		retry.Backoff(backoff.Constant(500*time.Millisecond), 500*time.Second),
--- aws/dynamodb/test/util_test.go
+++ aws/dynamodb/test/util_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	dynamodbv1 "github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/ory/dockertest"
 	"github.com/stretchr/testify/require"
 )
@@ -58,6 +59,60 @@ func TestStartDynamoDB(t *testing.T) {
 			"key": {S: aws.String("hello")},
 		},
 	}).Send(context.Background())
+	require.NoError(t, err)
+	require.NotNil(t, resp.Item["val"])
+	require.NotNil(t, resp.Item["val"].S)
+	require.Equal(t, "world", *resp.Item["val"].S)
+}
+
+func TestStartDynamoDBV1(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	db, cleanupFunc, err := StartDynamoDBV1(pool)
+	require.NoError(t, err)
+	defer cleanupFunc()
+
+	_, err = db.CreateTable(&dynamodbv1.CreateTableInput{
+		TableName: aws.String("test-table"),
+		KeySchema: []*dynamodbv1.KeySchemaElement{
+			{
+				AttributeName: aws.String("key"),
+				KeyType:       aws.String(dynamodbv1.KeyTypeHash),
+			},
+		},
+		AttributeDefinitions: []*dynamodbv1.AttributeDefinition{
+			{
+				AttributeName: aws.String("key"),
+				AttributeType: aws.String(dynamodbv1.ScalarAttributeTypeS),
+			},
+		},
+		ProvisionedThroughput: &dynamodbv1.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+	})
+	require.NoError(t, err)
+
+	_, err = db.PutItem(&dynamodbv1.PutItemInput{
+		TableName: aws.String("test-table"),
+		Item: map[string]*dynamodbv1.AttributeValue{
+			"key": {
+				S: aws.String("hello"),
+			},
+			"val": {
+				S: aws.String("world"),
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	resp, err := db.GetItem(&dynamodbv1.GetItemInput{
+		TableName: aws.String("test-table"),
+		Key: map[string]*dynamodbv1.AttributeValue{
+			"key": {S: aws.String("hello")},
+		},
+	})
 	require.NoError(t, err)
 	require.NotNil(t, resp.Item["val"])
 	require.NotNil(t, resp.Item["val"].S)
